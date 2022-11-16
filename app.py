d78951c20e6e815797720094630a389e871a645d
lucas <lucas@mail.com>
2022-11-16 17:00:22 -0400

    primeiro deploy

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.get('/')
+def index():
+    return 'app renderizado'
+
+@app.get('/inscrito/<nome_inscrito>')
+def inscrito(nome_inscrito):
+    return f"olá {nome_inscrito}"
+
+if __name__ == '__main__':
+    app.run()
